62c7db9fd7431be5b0f7068bb572466639e465d8
Jeremy Warner <jeremywrnr@gmail.com>
2017-07-18 18:37:07 -0700

    rebuild with year first option

--- lib/selector.rb
+++ lib/selector.rb
@@ -67,6 +67,7 @@ class Selector
     ad = a.downcase
     au = a.upcase
     return [
+      "#{y} #{a} #{t}.pdf",
       "#{a}_#{y}_#{t}.pdf".gsub(" ", "_"),
       "#{au}_#{y}_#{t}.pdf".gsub(" ", "_"),
       "#{ad}_#{y}_#{t}.pdf".gsub(" ", "_"),
