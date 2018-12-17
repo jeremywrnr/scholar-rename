fc413d1bd310830f4a54da0e2d87903ccc9dd556
Jeremy Warner <jeremywrnr@gmail.com>
2018-12-16 16:57:20 -0800

    add failing spec for --format

--- spec/renamer_spec.rb
+++ spec/renamer_spec.rb
@@ -3,12 +3,16 @@
 require 'spec_helper'
 
 describe Renamer do
-  it 'should show standard help when given no args' do
-    expect(nil).to eq nil
-  end
-
   it 'should show version number with -v' do
     out = Renamer.new '-v'
     expect(out.vers).to eq SR::Version
+  end
+
+  it 'should show format number with --format' do
+    raise false
+  end
+
+  it 'should use format number with --format [number]' do
+    raise false
   end
 end
